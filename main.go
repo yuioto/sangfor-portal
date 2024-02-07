ee139f74d8486627178df85327158ff6ff80d5b5
涼宮結音 <yuigpg@outlook.com>
2024-02-07 17:08:03 +0000

    feat(main): add captive portal detection and login functionality - Added asynchronous function 'checkCaptivePortalAsync' for detecting captive portal. - Implemented 'detectCaptivePortal' function to check for captive portal presence. - Created a placeholder 'login' function to be called when a captive portal is detected. - Added 'readConfig' function to read configuration from 'sangfor-portal' file. - Introduced a configuration file 'sangfor-portal.example.toml' as an example. - Updated log messages and formatting.

--- main.go
+++ main.go
@@ -1,45 +1,105 @@
 package main
 
 import (
-	"io/ioutil"
 	"net/http"
 	"os"
-	"strings"
 	"time"
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
 )
 
-func checkPortal(url string) {
-	response, err := http.Get(url)
-	if err != nil {
-		log.Warn().Msg("Request unsuccessful.")
-		return
-	}
-	defer response.Body.Close()
-
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Warn().Msg("Reading response failed.")
-		return
-	}
-
-	if response.StatusCode >= 300 && response.StatusCode < 400 && strings.Contains(string(body), "portal") {
-		log.Info().Msg("Portal detection succeed.")
-	} else {
-		log.Warn().Msg("Portal detection fail.")
-		log.Info().Msg("Network is online.")
-	}
-}
+const (
+	configFileName = "sangfor-portal"
+)
 
 func main() {
+	// 设置zerolog日志格式
 	// set log style, look like: 2006-01-02T15:04:05Z07:00 INFO Msg Str
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339})
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 
-	check_url := "https://ping.archlinux.org/nm-check.txt"
+	// 读取配置文件
+	const configFileName = "sangfor-portal"
+
+	err := readConfig()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to read config")
+	}
+
+	// check_url := "https://ping.archlinux.org/nm-check.txt"
+	url := "http://captive.apple.com/hotspot-detect.html"
+	//url := viper.GetString("url")
+	//account := viper.GetString("account")
+	//password := viper.GetString("password")
+	checkCaptivePortalAsync(url, login)
 
 	log.Info().Msg("アトリは、高性能ですから!")
 
-	checkPortal(check_url)
+	// 在这里可以继续执行其他任务，不会被阻塞
+	// 这里我简单地让程序等待一段时间来模拟其他任务
+	for {
+		time.Sleep(1000 * time.Second)
+	}
+}
+
+func checkCaptivePortalAsync(url string, loginFunc func()) {
+	go func() {
+		for {
+			if detectCaptivePortal(url) {
+				log.Info().Msg("Detected Captive Portal. Initiating login process")
+				loginFunc()
+				break
+			}
+			time.Sleep(5 * time.Second) // 每隔5秒检测一次
+		}
+	}()
+}
+
+func detectCaptivePortal(url string) bool {
+	resp, err := http.Get(url)
+
+	if err != nil {
+		log.Warn().Err(err).Msg("Error can't connect to network")
+		return false
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		return true
+	} else {
+		log.Warn().Err(err).Msg("Error while detecting Captive Portal")
+		return false
+	}
+
+}
+
+func login() {
+	log.Info().Msg("Login function called")
+	// 在这里执行登录操作
+	return
+}
+
+func readConfig() error {
+	// 设置配置文件搜索路径
+	viper.AddConfigPath("$HOME/.config") // 设置.config目录
+	viper.AddConfigPath("/etc/")         // 设置etc目录
+	viper.AddConfigPath(".")             // 设置当前目录
+	viper.SetConfigName(configFileName)  // 设置配置文件名称
+	viper.SetConfigType("toml")          // 设置配置文件类型为TOML
+
+	// 读取配置文件
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			log.Err(err).Msg("Config file not found")
+			return err
+		}
+		log.Warn().Err(err).Msg("Failed to read config file")
+		return err
+	}
+
+	log.Info().Msg("Config file loaded successfully")
+	return nil
 }
